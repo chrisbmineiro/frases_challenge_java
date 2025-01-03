ee137f590c9f688157e5ee36f8638a6c0f75e4db
chrisbmineiro <chrisbmineiro@gmail.com>
2025-01-02 21:30:48 -0300

    fix: arrumando a query do banco de dados

--- backend/src/main/java/com/frasesfilme/frases_filme/repository/FraseRepository.java
+++ backend/src/main/java/com/frasesfilme/frases_filme/repository/FraseRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface FraseRepository extends JpaRepository<Frase, Long> {
-    @Query("")
+    @Query("SELECT f FROM Frase f order by function('RANDOM') LIMIT 1")
     Frase buscaFraseAleatoria();
 }
